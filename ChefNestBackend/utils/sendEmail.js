dc357e90e3123c3f00ae479e191d322692e237e7
Sona1018 <sona196btcse23@igdtuw.ac.in>
2026-07-23 14:47:54 +0530

    Switch email service to Brevo SMTP

--- ChefNestBackend/utils/sendEmail.js
+++ ChefNestBackend/utils/sendEmail.js
@@ -1,16 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000, // 10 sec
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 const sendEmail = async (to, subject, html) => {
